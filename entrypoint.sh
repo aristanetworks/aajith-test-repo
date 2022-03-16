3d6ea713fcac10f8969d7782ca97d54de1a0c217
Arun Ajith S <aajith@arista.com>
2022-03-16 05:30:23 -0700

    Add ff-pr action to workflow: 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,15 @@ set -e
 echo "Target: ${GITHUB_BASE_REF}"
 echo "Source: ${GITHUB_HEAD_REF}"
 
-echo "Trying to merge(2) ${GITHUB_HEAD_REF} to base ${GITHUB_BASE_REF}"
-git merge origin/${GITHUB_HEAD_REF} --ff-only
+echo "Listing branches"
+git branch -a
+echo "Setup tracking branch"
+git branch --track ${GITHUB_HEAD_REF} origin/${GITHUB_HEAD_REF}
+echo "Tracking branch setup"
+echo "Listing branches again"
+git branch -a
+echo "Trying to merge(3) ${GITHUB_HEAD_REF} to base ${GITHUB_BASE_REF}"
+git merge ${GITHUB_HEAD_REF} --ff-only
 echo "Merge succeeded"
 echo "Trying to push"
 git push
