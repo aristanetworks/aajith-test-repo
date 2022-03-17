7a7d369b09197455cfd351d2bdb3aa797700d21b
Arun Ajith S <aajith@arista.com>
2022-03-16 22:37:00 -0700

    test merge with approval

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
-echo "Target: ${GITHUB_BASE_REF}"
-echo "Source: ${GITHUB_HEAD_REF}"
+[[ -n "${TARGET_BRANCH}" ]] || ( echo "TARGET_BRANCH undefined" >&2 && false )
+[[ -n "${SOURCE_BRANCH}" ]] || ( echo "SOURCE_BRANCH undefined" >&2 && false )
+
+echo "Target: ${TARGET_BRANCH}"
+echo "Source: ${SOURCE_BRANCH}"
 
 echo "Listing branches"
 git branch -a
-echo "Checking out base branch ${GITHUB_BASE_REF}"
-git checkout ${GITHUB_BASE_REF}
+
+echo "Checking out base branch ${TARGET_BRANCH}"
+git checkout ${TARGET_BRANCH}
 echo "Checkout successful"
-echo "Trying to merge(3) origin/${GITHUB_HEAD_REF} to base ${GITHUB_BASE_REF}"
-git merge origin/${GITHUB_HEAD_REF} --ff-only
+
+echo "Trying to merge origin/${SOURCE_BRANCH} to base ${TARGET_BRANCH}"
+git merge origin/${SOURCE_BRANCH} --ff-only
 echo "Merge succeeded"
+
 echo "Trying to push"
 git push
 echo "Push succeeded"
